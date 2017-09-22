c5a5369839cbfe1718bc5086a1e956bc3952bcaa
Kim Van Renterghem <kim.vanrenterghem@t4t.rocks>
2017-09-22 23:20:46 +0200

    miping

---
+++ CSharp.Pipe/CSharp.Pipe/Piping.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Pipe
+{
+    public static class Piping
+    {
+        public static TOut Pipe<TIn, TOut>(this TIn i, Func<TIn, TOut> f)
+            => f(i);
+        public static void Pipe<TIn>(this TIn i, Action<TIn> a)
+            => a(i);
+    }
+}
